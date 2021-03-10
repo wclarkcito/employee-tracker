5e82d3beeeab80bcaeb86cbbee89f602c967d89b
warren clark <wclarkcito@gmail.com>
2021-03-10 12:33:38 -0800

    connected to mySQL workbench

--- server.js
+++ server.js
@@ -1,4 +1,32 @@
+//Dependencies
+
 const mysql = require('mysql');
 const express = require('express');
 const fs = require('fs');
 const path = require("path");
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+
+    port: 3306,
+
+
+    user: 'root',
+
+
+    password: '1250Mesard!',
+    database: 'employee_db',
+});
+
+const afterConnection = () => {
+    connection.query('SELECT * FROM`employee_db`', (err, res) => {
+        if (err) throw err;
+        console.log(res);
+        connection.end();
+    })
+}
+connection.connect((err) => {
+    if (err) throw err;
+    console.log(`connected as id ${connection.threadId}`);
+    connection.end();
+});
