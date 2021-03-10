77954b778301af0f60994c77d4d6db3bddef769c
warren clark <wclarkcito@gmail.com>
2021-03-10 14:01:33 -0800

    modified mysql tables started inquirer.prompt functions

--- server.js
+++ server.js
@@ -2,19 +2,21 @@
 
 const mysql = require('mysql');
 const express = require('express');
-const fs = require('fs');
 const path = require("path");
+const inquirer = require('inquirer');
+const fs = require('fs');
+const table = require('console.table');
+
+
 
 const connection = mysql.createConnection({
     host: 'localhost',
 
     port: 3306,
 
-
     user: 'root',
-
-
     password: '1250Mesard!',
+
     database: 'employee_db',
 });
 
@@ -29,4 +31,41 @@ connection.connect((err) => {
     if (err) throw err;
     console.log(`connected as id ${connection.threadId}`);
     connection.end();
+
+    //showDept();
+
+    setTimeout(start, 1000);
 });
+
+function showDept() {
+    connection.query("SELECT * FROM departments", function (err, res, fields) {
+        if (err) throw err;
+        console.table(res);
+    })
+}
+
+function start() {
+    inquirer.prompt({
+        name: "start",
+        type: "list",
+        message: "Welcome please ksfsldflsf",
+        choices: ["Add Department", "Add Role", "List of Employees", "Exit"]
+    })
+        .then(function (answer) {
+            if (answer.start === "Add Department") {
+                addDepartment();
+            } else if (answer.start === "Add Role") {
+                console.log("role");
+            } else if (answer.start === "List of Employees") {
+                console.log("employees");
+            } else {
+                console.log("Thank you!")
+                connection.end();
+            }
+        })
+}
+
+function addDepartment() {
+    console.log("department");
+
+}
